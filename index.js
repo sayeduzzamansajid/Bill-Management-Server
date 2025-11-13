281c15e1b148f5eec01b8c59b46afd4ecc98067d
sayeduzzaman <sayeduzzamansajid@gmail.com>
2025-11-13 09:52:00 +0600

    Server running and '/' public limit(6) done

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://billManager:5aqEgCjlbM8LlGSw@orioncluster.vzkshzy.mongodb.net/?appName=orionCluster";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("Bill-Management");
+    const billCollection = db.collection("bills");
+
+    app.get('/', async (req, res) => {
+      try {
+        const result = await billCollection
+          .find()
+          .sort({ date: -1 })
+          .limit(6)
+          .toArray();
+          console.log(result);
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ message: "Error fetching bills" });
+      }
+    });
+
+    app.listen(port, () => console.log("Server running on port", port));
+  } catch (error) {
+    console.error(error);
+  }
+}
+run();
